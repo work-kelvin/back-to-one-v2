7e121631b4fff82b3735a23b9cfffe13a133fbc6
work-kelvin <work@kelvinaidoo.com>
2025-06-23 17:30:27 +0100

    refactor: replace page with working dashboard and remove problematic ui components

--- src/components/ui/input.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
-
-export const Input: React.FC<InputProps> = (props) => (
-  <input {...props} style={{ padding: '0.5rem', borderRadius: '4px', border: '1px solid #ccc' }} />
-); 
